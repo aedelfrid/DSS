337cb5b4ba4c3c423c7f2fe4b11dd3cc006b4bb6
aedelfrid <almck51@gmail.com>
2024-09-26 23:32:22 -0300

    notes

---
+++ parse.py
@@ -0,0 +1,27 @@
+# Parse
+
+import os
+
+# Need: 
+# read a file
+# write to file
+
+# CSV - JSON
+# read a file of a given type and format
+# convert to a format usable by python
+# ToCSV - ToJSON
+# write a file of a given type or format
+# convert from python string to new format
+
+# CSV - 
+# Domains, check for scanned status
+
+# "www.sample.com, title, status\n"
+
+def 
+
+
+# JSON
+
+# select range from domain list
+# random sample - set sample size, seed, select which info to include in csv - good for research/study purposes?
